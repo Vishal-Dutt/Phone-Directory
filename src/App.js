78c5e78d484d3fffe754490237440fc4caec7d6c
Vishal Dutt <vishaldutt157@gmail.com>
2020-08-18 20:28:51 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React , { Component } from 'react';
+import { Fragment } from 'react';
+import Header from "./Header";
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+    let subscribers = [
+      {
+        id: 1,
+        name: "Vishal Dutt",
+        phone: "8888888888"
+      },
+      {
+        id: 2,
+        name: "Vishal Dutt",
+        phone: "9999999999"
+      }
+    ]
+    return (
+      <Fragment>
+        <Header heading="Phone Directory"/>
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+
+          {
+            subscribers.map(sub => {
+              return <div key={sub.id} className="grid-container">
+              <span className="grid-item">{sub.name}</span>
+              <span className="grid-item">{sub.phone}</span>
+              <span className="grid-item action-btn-container">
+                 <button className="custom-btn delete-btn">Delete</button>
+                </span>  
+              </div>
+            })
+          }
+        </div> 
+      </Fragment>
+    );
+  }
+} 
+
 
 export default App;
